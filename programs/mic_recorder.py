732e6e6bc007bcabb5ca1bcd1f3ae57d729e30aa
Marc Pelletier <128648964+Goude63@users.noreply.github.com>
2024-01-11 11:34:47 -0500

    Add files via upload
    Microphone recorder using DMA, and SD card + Labview utility to plaback and convert to wave files

---
+++ programs/mic_recorder.py
@@ -0,0 +1,69 @@
+#
+#  Record microphone ADC ch 0 to SD Card (Waveshare pico zero)
+#  UI: Press button to start/stop recording (red led = record)
+#      Long press button to quit. Led off when program ends
+#  Also file auto split every hour. Led blue = stand-by/not recording
+#  SD Card on SPI0 GP0-GP3, button on GP9 -> gnd 
+#  GP26: Mic with DC offset ~1.25V @ ~2.5V pk2pk (on loudest sound)
+
+from adc import Adc
+from sdcard import SDCard
+from machine import Pin
+import time, sys
+from utils import *
+from led import LED
+
+def Next_fn(i):
+	return '/sd/rec' + '{:03.0f}'.format(i) + '.bin'
+
+def main():
+	machine.freq(270000000)
+	led = LED()
+	SDCard.Mount()
+
+	# if recordings are there, skip files
+	fno = 1
+	while exists(Next_fn(fno)): 
+		fno += 1
+
+	adc = Adc(chs=0, fs = 22000, buf_size=2048)
+	btn_up = Pin(9,Pin.IN,Pin.PULL_UP) # ground GPIO8 to stop
+
+	led.off()
+	run = True
+	while run:
+		led.on(0,0,16) # blue = stand by
+		n = 0
+		while n < 15 and not btn_up(): # wait btn up
+			n += 1
+			time.sleep_ms(100)
+		while btn_up(): pass		# wait press
+		led.on(20,20,0)
+		while not btn_up(): # short press = start record
+			n += 1			# long press = quit
+			if n == 15: led.on(5,5,5) # dim=>quit soon
+			if n == 30: 
+				led.off() # exit: led off
+				run = False
+				break
+			else: time.sleep_ms(100)
+
+		record = run
+		r = 16
+		while record:
+			fn = Next_fn(fno)
+			f = open(fn, "w")
+			adc.WriteFileHdr(f)
+			adc.Start(file=f)
+			for n in range(3600): # 1 hrs per file
+				if btn_up():
+					led.on(r,0,0) # red blink when recording
+					r = 16 - r
+					time.sleep(1)
+				else:
+					record = False
+			adc.Stop()
+			f.close()
+			fno += 1
+
+main()
